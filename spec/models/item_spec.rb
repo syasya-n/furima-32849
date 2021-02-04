25ef5bf6b03a0da91ca41fe25574dfcd42faaa43
syasya-n <yousuihiromi@gmail.com>
2021-02-04 22:08:21 +0900

    コメントアウト整理

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -40,31 +40,31 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include("Category can't be blank")
       end
-      
+
       it 'カテゴリーが初期値"---"では登録できないこと' do
         @item.category_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include('Category Select')
       end
-      
+
       it '商品の状態が空では登録できないこと' do
         @item.condition_id = nil
         @item.valid?
         expect(@item.errors.full_messages).to include("Condition can't be blank")
       end
-      
+
       it '商品の状態が初期値"---"では登録できないこと' do
         @item.condition_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include('Condition Select')
       end
-           
+
       it '配送料の負担が空では登録できないこと' do
         @item.shipping_fee_person_id = nil
         @item.valid?
         expect(@item.errors.full_messages).to include("Shipping fee person can't be blank")
       end
-           
+
       it '配送料の負担が初期値"---"では登録できないこと' do
         @item.shipping_fee_person_id = 1
         @item.valid?
