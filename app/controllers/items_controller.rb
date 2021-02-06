808f5374a2a7e33f1bea896a681c2447117aa1d6
syasya-n <yousuihiromi@gmail.com>
2021-02-06 19:34:54 +0900

    レビュー指摘対応
    コントローラ共通処理集約
    destroyメソッド修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new]
+  before_action :set_item, only: [:destroy,:show]
 
   def index
     @items = Item.includes(:user).order('created_at DESC')
@@ -19,18 +20,26 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    @item = Item.find(params[:id])
-    @item.destroy
-    redirect_to root_path
+    if current_user.id == @item.user.id
+      if @item.destroy
+        redirect_to root_path
+      else
+        render :show
+      end
+    end
   end
 
   def show
-    @item = Item.find(params[:id])
   end
-    private
+  
+  private
 
   def item_params
     params.require(:item).permit(:image, :name, :description, :category_id, :condition_id, :shipping_fee_person_id,
                                  :ship_from_region_id, :delivery_day_id, :price).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
