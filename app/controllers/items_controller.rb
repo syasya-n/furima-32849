69bb308ed918b8a8411b2f696af2582be879f0a8
syasya-n <yousuihiromi@gmail.com>
2021-02-06 19:25:36 +0900

    追加修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -23,7 +23,7 @@ class ItemsController < ApplicationController
       if @item.destroy
         redirect_to root_path
       else
-        render :index
+        render :show
       end
     end
   end
