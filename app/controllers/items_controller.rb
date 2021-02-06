1c87ec0dcf858a789522b21fe0ec77200c8e7efd
syasya-n <yousuihiromi@gmail.com>
2021-02-06 17:06:46 +0900

    商品削除機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -18,6 +18,12 @@ class ItemsController < ApplicationController
     end
   end
 
+  def destroy
+    @item = Item.find(params[:id])
+    @item.destroy
+    redirect_to root_path
+  end
+
   def show
     @item = Item.find(params[:id])
   end
