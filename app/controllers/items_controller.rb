912a93ca311646288b524b6c0a1710b818122897
syasya-n <yousuihiromi@gmail.com>
2021-02-06 15:51:56 +0900

    商品詳細表示機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -18,6 +18,14 @@ class ItemsController < ApplicationController
     end
   end
 
+  def show
+    @item = Item.find(params[:id])
+    @category = Category.data.find{|o| o[:id] == @item.category_id}
+    @condition = Condition.data.find{|o| o[:id] == @item.condition_id}
+    @delivery_day = DeliveryDay.data.find{|o| o[:id] == @item.delivery_day_id}
+    @ship_from_region = ShipFromRegion.data.find{|o| o[:id] == @item.ship_from_region_id}
+    @shipping_fee_person = ShippingFeePerson.data.find{|o| o[:id] == @item.shipping_fee_person_id}
+  end
   private
 
   def item_params
