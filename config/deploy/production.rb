7e30bbbe2580b75df2d5e729a9512cdcdcf83edc
syasya-n <yousuihiromi@gmail.com>
2021-03-20 15:57:59 +0900

    typo修正

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -59,4 +59,4 @@
 #     auth_methods: %w(publickey password)
 #     # password: "please use keys"
 #   }
-serrver '54.168.11.203',user: 'ec2-user', roles: %w{app db web}
+server '54.168.11.203',user: 'ec2-user', roles: %w{app db web}
